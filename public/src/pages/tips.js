b9aae77e68ef9dae4bcc654d1c9be0e6e7e74068
dengxinjuan <dengxinj@gmail.com>
2021-10-01 19:53:19 -0700

    test item point

--- public/src/pages/tips.js
+++ public/src/pages/tips.js
@@ -3,8 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const IMAGE_URL = "https://spoonacular.com/cdn/ingredients_250x250/";
-const SEARCH_URL =
-  "https://cors-anywhere.herokuapp.com/https://shelf-life-api.herokuapp.com/search";
+const SEARCH_URL = "https://food-ping.herokuapp.com/searchItem";
 
 /*  the search_url always show cors error i dont know what to do
  */
@@ -16,10 +15,10 @@ function Tips() {
   useEffect(() => {
     async function getID(name) {
       const { data } = await axios.get(SEARCH_URL, {
-        params: { q: name },
+        params: { item: name },
       });
       console.log(data);
-      setDetails(data);
+      setDetails(data[0].id);
     }
     getID(name);
   }, [name]);
@@ -30,7 +29,7 @@ function Tips() {
 
       <img src={`${IMAGE_URL}/${name}`} alt={name} />
       <h2>Did you know?</h2>
-      <h3>{details} tips supposed to be here but cors error</h3>
+      <h3>{details} </h3>
       <button>MARK US USED</button>
     </div>
   );
