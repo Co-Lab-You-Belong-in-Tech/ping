fcc54c55e4cdbc9ed1a6d16888f740a2f4d7c5a0
dengxinjuan <dengxinj@gmail.com>
2021-10-02 13:26:28 -0700

    search autocomplete but need a new route

--- public/src/pages/search.js
+++ public/src/pages/search.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function SearchPage() {
-  return <div>this is search page</div>;
+  const [veggie, setVeggie] = useState([]);
+  const [text, setText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    async function getVeggie() {
+      const { data } = await axios.get(
+        "https://food-ping.herokuapp.com/searchItem", // i need a route to provide same data as https://shelf-life-api.herokuapp.com/search
+        {
+          params: { item: "apple" },
+        }
+      );
+      console.log(data);
+      setVeggie(data);
+    }
+    getVeggie();
+  }, []);
+
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length > 0) {
+      matches = veggie.filter((a) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return a.name.match(regex);
+      });
+    }
+    console.log(matches);
+    setSuggestions(matches);
+    setText(text);
+  };
+
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  };
+  return (
+    <div>
+      this is search page
+      <input
+        type="text"
+        onChange={(e) => onChangeHandler(e.target.value)}
+        value={text}
+      />
+      {suggestions &&
+        suggestions.map((suggestion) => (
+          <div
+            key={suggestion.id}
+            onClick={() => onSuggestHandler(suggestion.name)}
+          >
+            {suggestion.id}--{suggestion.name}
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default SearchPage;
