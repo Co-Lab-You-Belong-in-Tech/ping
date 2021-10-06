1c61b1d2edccd520265cb769ff06102ed9acd16f
lyanita <anitaly.ubc@gmail.com>
2021-10-05 22:50:37 -0700

    Added input validation and cron scheduler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var dotenv = require('dotenv').config();
 var schedule = require('node-schedule');
 
 var handlebars = require('express-handlebars').create({
@@ -13,7 +14,7 @@ app.set('view engine', 'handlebars');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
-  }));
+}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -25,20 +26,22 @@ app.use((req, res, next) => {
 /*Database*/
 const {
     Client
-  } = require('pg');
+} = require('pg');
+
 
 const pool = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
+        rejectUnauthorized: false
     }
-  });
-  
+});
+
+
 pool.connect();
 
 /*Routes*/
-app.get('/', function(req, res) {
-    res.send("Test");
+app.get('/', function (req, res) {
+    res.send("PING ME.");
 });
 
 //Description: proxy for Still Tasty (Shelf-Life) API
@@ -46,11 +49,15 @@ app.get('/', function(req, res) {
 app.get('/searchItem', (req, res) => {
     var item = req.query.item;
     console.log(item);
-    request(
-        {url: 'https://shelf-life-api.herokuapp.com/search?q=' + item},
+    request({
+            url: 'https://shelf-life-api.herokuapp.com/search?q=' + item
+        },
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
-                return res.status(500).json({ type: 'error', message: error.message});
+                return res.status(500).json({
+                    type: 'error',
+                    message: error.message
+                });
             }
             res.json(JSON.parse(body));
         }
@@ -62,11 +69,15 @@ app.get('/searchItem', (req, res) => {
 app.get('/getDetails', (req, res) => {
     var query_id = req.query.query_id;
     console.log(query_id);
-    request(
-        {url: 'https://shelf-life-api.herokuapp.com/guides/' + query_id},
+    request({
+            url: 'https://shelf-life-api.herokuapp.com/guides/' + query_id
+        },
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
-                return res.status(500).json({ type: 'error', message: error.message});
+                return res.status(500).json({
+                    type: 'error',
+                    message: error.message
+                });
             }
             res.json(JSON.parse(body));
         }
@@ -77,7 +88,7 @@ app.get('/getDetails', (req, res) => {
 //Parameters: user_id (int)
 app.get('/getUser', async (req, res) => {
     try {
-        await pool.query('SELECT * FROM users WHERE user_id=$1', [req.query.user_id], 
+        await pool.query('SELECT * FROM users WHERE user_id=$1', [req.query.user_id],
             function (err, result, fields, rowCount) {
                 console.log(result);
                 console.log(result.rows);
@@ -95,7 +106,7 @@ app.get('/getUser', async (req, res) => {
 //Parameters: None
 app.get('/getList', async (req, res) => {
     try {
-        await pool.query('SELECT DISTINCT (item_name) FROM inventory', function (err, result, fields, rowCount) { 
+        await pool.query('SELECT DISTINCT (item_name) FROM inventory', function (err, result, fields, rowCount) {
             console.log(result);
             let string = JSON.stringify(result.rows);
             res.send(string);
@@ -110,12 +121,13 @@ app.get('/getList', async (req, res) => {
 //Parameters: user_id (int)
 app.get('/getInventory', async (req, res) => {
     try {
-        await pool.query('SELECT * FROM inventory WHERE user_id=$1', 
-            [req.query.user_id], function (err, result, fields, rowCount) {
-            console.log(result);
-            let string = JSON.stringify(result.rows);
-            res.send(string);
-        });
+        await pool.query('SELECT * FROM inventory WHERE user_id=$1',
+            [req.query.user_id],
+            function (err, result, fields, rowCount) {
+                console.log(result);
+                let string = JSON.stringify(result.rows);
+                res.send(string);
+            });
     } catch (err) {
         console.error(err);
         res.send("Error: " + err);
@@ -146,7 +158,7 @@ app.post('/addItem', async (req, res, next) => {
         var expiry_date = addDate(req.query.input_date, expiry_time); //calculates the expiry_date by adding the expiry_time to input_date
         //console.log(expiry_date);
         var amount_used = 0; //default amount used to 0
-        var tag = "not expired"; //default tag to not expired
+        var tag = "not expired"; //default tag to not expired (tag is enum('not expired', 'expired', 'finished'))
         await pool.query('INSERT INTO inventory (item_name, user_id, original_amount, amount_used, input_date, expiry_date, tag, query_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
             [req.query.item_name, req.query.user_id, req.query.original_amount, amount_used, req.query.input_date, expiry_date, tag, req.query.query_id],
             function (err, result) {
@@ -164,15 +176,22 @@ app.post('/addItem', async (req, res, next) => {
 //Parameters: amount_used (int), user_id (int), item_id (int)
 app.put('/editItem', async (req, res, next) => {
     try {
-        await pool.query('UPDATE inventory SET amount_used = $1 WHERE user_id = $2 AND item_id = $3', //apply user validation where amount_used <= original_amount
-            [req.query.amount_used, req.query.user_id, req.query.item_id],
+        await pool.query('SELECT * FROM inventory WHERE user_id = $1 AND item_id = $3', [req.query.user_id, req.query.item_id],
             function (err, result) {
-                let string = JSON.stringify(result);
-                console.log(result);
-                res.send(string);
-            }
-        );
-        await checkAmount();
+                var record = result.rows;
+                var original_amount = record.original_amount;
+                if (amount_used <= original_amount) { //only update if amount_used is a valid amount i.e. less than or equal to original_amount
+                    pool.query('UPDATE inventory SET amount_used = $1 WHERE user_id = $2 AND item_id = $3',
+                        [req.query.amount_used, req.query.user_id, req.query.item_id],
+                        function (err, result) {
+                            let string = JSON.stringify(result);
+                            console.log(result);
+                            res.send(string);
+                        }
+                    );
+                }
+            });
+        await checkAmount(); //check if amount_used equals to original_amount
     } catch (err) {
         console.error(err);
         res.send("Error: " + err);
@@ -182,7 +201,7 @@ app.put('/editItem', async (req, res, next) => {
 /*Utils*/
 //Description: convert the time in seconds to days
 const convertDate = (expiry_time) => {
-    var shelf_life = Math.round(expiry_time / (60*60*24));
+    var shelf_life = Math.round(expiry_time / (60 * 60 * 24));
     //console.log("Shelf Life: " + shelf_life);
     return shelf_life;
 }
@@ -193,7 +212,7 @@ const addDate = (input_date, expiry_time) => {
     //console.log("Input Date: " + current_date);
     const new_date = current_date.setDate(current_date.getDate() + expiry_time);
     const expiry_date = new Date(new_date);
-    const expiry_date_formatted = expiry_date.toISOString().split('T')[0];
+    const expiry_date_formatted = expiry_date.toISOString().split('T')[0]; //retains the date portion of datetime
     console.log("Expiry Date Formatted: " + expiry_date_formatted);
     return expiry_date_formatted;
 }
@@ -206,31 +225,52 @@ const checkAmount = (original_amount, amount_used, user_id, item_id) => {
     }
 }
 
-//Description: update tag to finished
+//Description: update tag to given tag parameter
 const updateTag = (tag, user_id, item_id) => {
     pool.query('UPDATE inventory SET tag = $1 WHERE user_id = $2 AND item_id = $3', [tag, user_id, item_id],
         function (err, result) {
             let string = JSON.stringify(result);
             console.log(result);
-            res.send(string);
         }
     );
 }
 
-//Description: get array of item_ids
+//Description: get array of item_id key-value pairs
 const getItemIDs = async () => {
     await pool.query('SELECT DISTINCT item_id FROM inventory ORDER BY item_id ASC', function (err, result) {
-        var id_arr = [];
-        console.log(result);
+        var records = result.rows;
+        //console.log(records);
+        return records;
     });
 }
 
 //Description: schedule a check to update the tag to expired if the expiry date is passed
-var expiryCheck = schedule.scheduleJob('0 0 * * *', function() {
+var expiryCheck = async () => {
     console.log("Test scheduler.");
-});
+    await pool.query('SELECT * FROM inventory', function (err, result) {
+        var records = result.rows;
+        records.forEach((row) => {
+            var item_id = row.item_id;
+            var user_id = row.user_id;
+            var expiry_date = row.expiry_date;
+            var today_date = new Date();
+            var current_tag = row.tag;
+            if (today_date >= expiry_date && current_tag === 'not expired') { //update tag to expired if expiry date has passed and item is not completely used
+                var tag = 'expired';
+                updateTag(tag, user_id, item_id);
+            }
+        });
+    });
+};
 
-getItemIDs();
+var rule = new schedule.RecurrenceRule();
+rule.hour = 0; //runs once a day at midnight
+//rule.minute = new schedule.Range(0, 59, 1);
+
+const expiryCheckScheduler = schedule.scheduleJob(rule, function () { //runs function based on CRON scheduler
+    console.log("Finished expiry check.");
+    expiryCheck();
+});
 
 /*Error Handling*/
 
