54e67c7e5c1cb300e14a0da2db8bc6ee11e3f402
dengxinjuan <dengxinj@gmail.com>
2021-10-07 13:13:22 -0700

    add email validation for login form

--- public/src/pages/home.js
+++ public/src/pages/home.js
@@ -1,26 +1,68 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../UserContext";
+import validator from "validator";
 import { login } from "./testLogin";
 
 function HomePage() {
-  const [email, setEmail] = useState(" ");
-  const { user, setUser } = useContext(UserContext);
+  const initialState = { email: "" };
+  const [formData, setFormData] = useState(initialState);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+    validateEmail(e);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email } = formData;
+    alert(`${email}`);
+    setFormData(initialState);
+
+    //add some kind of login logic here
+  };
+
+  const [emailError, setEmailError] = useState("");
+  const validateEmail = (e) => {
+    var email = e.target.value;
+
+    if (validator.isEmail(email)) {
+      setEmailError("Valid Email :)");
+    } else {
+      setEmailError("Enter valid Email!");
+    }
+  };
+
+  const { user, setUser } = useContext(UserContext); // use useContext to grab user id
+
   return (
     <div>
-      <h1>Ping!</h1>
+      <h1>KARROT</h1>
       <Link to="/inventory">Inventory</Link>
 
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
           <input
-            type="text"
-            value={email}
-            placeholder={"Enter Your Email"}
-            onChange={({ target }) => setEmail(target.value)}
+            type="email"
+            placeholder="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={handleChange}
           />
-
-          <button type="submit">
+          <span
+            style={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+          >
+            {emailError}
+          </span>
+          <button>
             <Link to="/search">Start!</Link>
           </button>
         </form>
