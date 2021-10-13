460d7ad9121a567c6ed10eb58495df47339f602a
dengxinjuan <dengxinj@gmail.com>
2021-10-13 15:54:13 -0700

    add handlebought but need to calcultae time

--- public/src/components/SingleGrocery.js
+++ public/src/components/SingleGrocery.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import OwnAPI from "../Api";
+
 const SingleGrocery = ({
   grocery_item_id,
   grocery_item_name,
@@ -5,9 +8,23 @@ const SingleGrocery = ({
   display_tag,
   query_id = 16623,
 }) => {
+  /*handle Bought function*/
+  async function handleBought(
+    item_name,
+    user_id,
+    expiry_time = "12232132",
+    query_id = "16808"
+  ) {
+    OwnAPI.addFridge(item_name, user_id, expiry_time, query_id);
+  }
+
+  // need some functions to get expiry_time
+
   return (
     <div key={grocery_item_id}>
-      <button>{(grocery_tag = "not bought" ? "x" : "y")}</button>
+      <button onClick={() => handleBought(grocery_item_name, "5", query_id)}>
+        {grocery_tag === "not bought" ? "x" : <span>&#10003;</span>}
+      </button>
       {grocery_item_name}
       -----{query_id}
     </div>
