d4e08813388addde23a4e0c570b5dfb7f88d9d55
dengxinjuan <dengxinj@gmail.com>
2021-10-15 19:41:36 -0700

    call recipes on api

--- public/src/pages/recipes.js
+++ public/src/pages/recipes.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
 import NavBar from "../components/NavBar";
 import receipeLogo from "../assets/Recipe_Logo.png";
 import FridgeHolder from "../components/empty_holder/Fridge_holder";
 import EmptyRecipe from "../assets/Empty_Recipe_Logo.png";
+import UserContext from "../UserContext";
 
 const RecipesPage = () => {
+  const [recipeData, setRecipeData] = useState(null);
+  const { expiredItem } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getRecipe() {
+      const { data } = await axios.get(
+        "https://food-ping.herokuapp.com/getRecipes",
+        {
+          params: { ingredients: expiredItem, number: "4" },
+        }
+      );
+      console.log(data);
+      setRecipeData(data);
+    }
+    getRecipe();
+  }, []);
+
   return (
     <div>
       <img src={receipeLogo} />
       <h1>My Recipes</h1>
       <p>Based on ingredients Left</p>
-      <FridgeHolder
-        img={EmptyRecipe}
-        title={"You dont have any receipes yet"}
-        message={"Add item to your fridge to see recipe ideas"}
-      />
+      {!recipeData && (
+        <FridgeHolder
+          img={EmptyRecipe}
+          title={"You dont have any receipes yet"}
+          message={"Add item to your fridge to see recipe ideas"}
+        />
+      )}
     </div>
   );
 };
