55f233e0b91b3bd682bcd46b5559966ba1e1d3d4
dengxinjuan <dengxinj@gmail.com>
2021-10-20 15:59:34 -0700

    notification mark as used work

--- public/src/components/FridgeArea.js
+++ public/src/components/FridgeArea.js
@@ -35,7 +35,27 @@ const FridgeArea = ({ userData }) => {
     <div>
       <button
         className="btn-large"
-        onClick={() => handleUsageTag("used", user, tagArray)}
+        onClick={() => {
+          store.addNotification({
+            content: (
+              <LargeNtf
+                tagArray={tagArray}
+                handleUsageTag={handleUsageTag}
+                user={user}
+              />
+            ), // content:MyNotify (custom notification), pass value and function into
+            type: "success",
+            insert: "top",
+            container: "center",
+            // animationIn: ["animate__animated", "animate__fadeIn"],
+            //animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+              duration: 2000,
+              onScreen: true,
+              showIcon: true,
+            },
+          });
+        }}
       >
         MARK AS USED
       </button>
--- public/src/components/Notifications/largeNotify.js
+++ public/src/components/Notifications/largeNotify.js
@@ -1,9 +1,14 @@
-const LargeNtf = () => {
+const LargeNtf = ({ tagArray, handleUsageTag, user }) => {
   return (
     <div className="largeNtf">
-      <div></div>
+      <div>{tagArray}</div>
       <h5 className="nty-font">Are you sure you want to mark as used?</h5>
-      <button className="nty-btn">MARK AS USED</button>
+      <button
+        className="nty-btn"
+        onClick={() => handleUsageTag("used", user, tagArray)}
+      >
+        MARK AS USED
+      </button>
       <button className="nty-btn-danger">NEVERMIND</button>
     </div>
   );
