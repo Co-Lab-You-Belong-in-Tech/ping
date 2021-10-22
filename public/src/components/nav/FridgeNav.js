f3ae73fd011683c2bc8b7bb2fd173be39c8b0053
dengxinjuan <dengxinj@gmail.com>
2021-10-22 11:21:42 -0700

    add notification part

--- public/src/components/nav/FridgeNav.js
+++ public/src/components/nav/FridgeNav.js
@@ -1,8 +1,33 @@
 import { store } from "react-notifications-component";
 import { ReactComponent as FridegeUn } from "../../assets/FridgeUnselected.svg";
+import LargeExp from "../Notifications/largeExp";
 
 const FridgeNav = ({ expiredItem }) => {
-  return <div>{expiredItem.length > 0 ? "FridgeA" : <FridegeUn />}</div>;
+  /**this is the react notifications part */
+
+  const handleClick = () => {
+    store.addNotification({
+      content: <LargeExp expireItems={expiredItem} />, // content:MyNotify (custom notification)
+      type: "success",
+      insert: "top",
+      container: "center",
+      animationIn: ["animate__animated", "animate__fadeIn"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 2000,
+        onScreen: true,
+      },
+    });
+  };
+  return (
+    <div>
+      {expiredItem.length > 0 ? (
+        <div onClick={() => handleClick()}>"FridgeA"</div>
+      ) : (
+        <FridegeUn />
+      )}
+    </div>
+  );
 };
 
 export default FridgeNav;
