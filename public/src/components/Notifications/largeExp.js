5bc71fdd1ad2e6dbc2c777346f4bc09e4c606216
dengxinjuan <dengxinj@gmail.com>
2021-10-23 21:13:53 -0700

    arrange large notification layout

--- public/src/components/Notifications/largeExp.js
+++ public/src/components/Notifications/largeExp.js
@@ -3,8 +3,8 @@ import { wrap } from "module";
 const LargeExp = ({ expireItems }) => {
   return (
     <div className="largeNtf">
-      <div style={{ overflowWrap: "break-word" }}>
-        <p className="nty-font">Your {expireItems} are about to expire!</p>
+      <div style={{ paddingTop: "18%", paddingBottom: "10%" }}>
+        <h5 className="nty-font">Your {expireItems} are about to expire!</h5>
       </div>
       <div style={{ padding: "10px", display: "inline-block" }}>
         <button className="nty-btn">GOT IT!</button>
