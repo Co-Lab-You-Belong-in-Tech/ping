7f45a6e998d4bff8e6893f03018743a2305c0c38
dengxinjuan <dengxinj@gmail.com>
2021-10-25 12:13:38 -0700

    add "," in notification

--- public/src/components/Notifications/largeExp.js
+++ public/src/components/Notifications/largeExp.js
@@ -2,7 +2,9 @@ const LargeExp = ({ expireItems }) => {
   return (
     <div className="largeNtf">
       <div style={{ paddingTop: "18%", paddingBottom: "10%" }}>
-        <h5 className="nty-font">Your {expireItems} are about to expire!</h5>
+        <h5 className="nty-font">
+          Your {expireItems.join()} are about to expire!
+        </h5>
       </div>
       <div style={{ padding: "10px", display: "inline-block" }}>
         <button className="nty-btn">GOT IT!</button>
