1e07bf9b510dd1178ecc257cdf27c29d855919eb
dengxinjuan <dengxinj@gmail.com>
2021-10-25 12:31:23 -0700

    expire notification add

--- public/src/components/Notifications/largeExp.js
+++ public/src/components/Notifications/largeExp.js
@@ -3,7 +3,7 @@ const LargeExp = ({ expireItems }) => {
     <div className="largeNtf">
       <div style={{ paddingTop: "18%", paddingBottom: "10%" }}>
         <h5 className="nty-font">
-          Your {expireItems.join()} are about to expire!
+          Your {expireItems.join(" and ")} are about to expire!
         </h5>
       </div>
       <div style={{ padding: "10px", display: "inline-block" }}>
