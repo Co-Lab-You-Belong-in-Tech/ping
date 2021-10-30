7d421d3672318f077954fe04e4359578cd898f92
dengxinjuan <dengxinj@gmail.com>
2021-10-29 22:41:48 -0700

    add the load more button to inventory page

--- public/src/pages/inventory.js
+++ public/src/pages/inventory.js
@@ -6,9 +6,15 @@ import logo from "../assets/Fridge_Logo.png";
 import BottomNavBar from "../components/nav/BottomNavBar";
 import Helper from "../hooks/Helpers";
 
+const LIMIT = 5;
+
 function Inventory() {
   const { user, setExpiredItems } = useContext(UserContext);
   const [userData, setUserData] = useState(null);
+  const [showMore, setShowMore] = useState(true);
+  const [list, setList] = useState(null);
+  const [index, setIndex] = useState(LIMIT);
+
   useEffect(() => {
     async function getData() {
       try {
@@ -26,6 +32,10 @@ function Inventory() {
         );
 
         setUserData(sortedA);
+        setList(sortedA.slice(0, LIMIT));
+        if (sortedA.length <= LIMIT) {
+          setShowMore(false);
+        }
       } catch (e) {
         console.error(e);
       }
@@ -34,8 +44,19 @@ function Inventory() {
   }, []);
 
   // loop through the data and find the expiration item and put them into array and set the expiration item appwide
-
   // logic:  if the itemtag is not expired , display here; if user click toss or used buttom they disapper from the page but change the tag in database
+
+  /**LOAD MORE BUTTON */
+
+  const loadMore = () => {
+    const newIndex = index + LIMIT;
+    const newShowMore = newIndex < userData.length - 1;
+    const newList = list.concat(userData.slice(index, newIndex));
+    setIndex(newIndex);
+    setList(newList);
+    setShowMore(newShowMore);
+  };
+
   return (
     <div>
       <div className="header-box">
@@ -51,7 +72,37 @@ function Inventory() {
         </div>
       </div>
 
-      <FridgeArea userData={userData} />
+      <FridgeArea userData={list} />
+
+      {showMore && (
+        <button onClick={loadMore} className="load-more">
+          <svg
+            width="14"
+            height="8"
+            viewBox="0 0 14 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="12.4766"
+              y="0.00683594"
+              width="1.69288"
+              height="9.62083"
+              rx="0.84644"
+              transform="rotate(45.1707 12.4766 0.00683594)"
+              fill="#2A9D8F"
+            />
+            <rect
+              y="1.19678"
+              width="1.69288"
+              height="9.62083"
+              rx="0.84644"
+              transform="rotate(-45 0 1.19678)"
+              fill="#2A9D8F"
+            />
+          </svg>
+        </button>
+      )}
       <BottomNavBar name="inventory" />
     </div>
   );
